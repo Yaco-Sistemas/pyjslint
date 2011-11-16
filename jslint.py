055c2a1aefeb775dc44c5b4eabd58562bab7a5c9
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2011-11-16 14:23:04 +0100

    Remove the reformat function since it's better to see the actual code line

--- jslint.py
+++ jslint.py
@@ -9,13 +9,11 @@ wrapper for JSLint
 from optparse import OptionParser
 import os
 import urllib2
-import re
 import sys
 import subprocess
 from tempfile import NamedTemporaryFile
 
 
-pattern = r"Lint at line (\d+) character (\d+): (.*)"
 default_jslint_options = r"""
 vars: true,
 maxerr: 100,
@@ -100,19 +98,6 @@ def process(jsfile, options):
     return [line for line in output.split("\n") if line]
 
 
-def reformat(lines, pattern, filename):
-    results = []
-    regex = re.compile(pattern)
-    for line in lines:
-        matches = regex.search(line)
-        if matches:
-            line = int(matches.groups()[0])
-            char = int(matches.groups()[1])
-            msg = matches.groups()[2]
-            results.append("%s - %d:%d - %s" % (filename, line, char, msg))
-    return results
-
-
 # Hooks entry point
 def check_JSLint(code_string):
     tmpfile = NamedTemporaryFile()
@@ -131,7 +116,7 @@ def main():
     filename = args[0]
     errors = process(open(filename, "r"), options)
     if len(errors) > 0:
-        print "\n".join(reformat(errors, pattern, filename))
+        print "\n".join(errors)
         sys.exit(False)
     sys.exit(True)
 
